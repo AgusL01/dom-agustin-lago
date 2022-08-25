12d6c96a4c6d8c4311c46bdc05274e5f60755412
agusl01 <agustinlago908@gmail.com>
2022-08-25 17:08:12 -0300

    agregodommm

---
+++ .history/js/main_20220825145227.js
@@ -0,0 +1,7 @@
+/*se declaran en variables el valor de cada moneda*/
+
+    let dolar = 300
+    let euro = 250
+    let libra = 350
+
+let monedaDolar = getElemenById('dolar')
---
+++ .history/js/main_20220825151953.js
@@ -0,0 +1,13 @@
+/*se declaran la funcion a realizar*/
+function conversor(){
+    let valor = parseInt(document.getElemenById('montoCotizar').value);
+    let montoCambiar = montoCotizar;
+    /*declaramos el valor de las monedas*/
+    let dolar = 300;
+    let euro = 250;
+    let libra = 350;
+    if (document.getElementById(uno)){
+    let montoCambiado= montoCambiar / dolar
+    }
+}
+
